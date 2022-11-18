11804811739008afd7faa8fc65a5438b8e749e8a
Jose <jllopezl07@gmail.com>
2022-11-18 14:05:27 +0100

    Configure output reports

--- src/test/java/com/example/cucumber/automation/RunCucumberTest.java
+++ src/test/java/com/example/cucumber/automation/RunCucumberTest.java
@@ -11,7 +11,7 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber.html,json:target/cucumber.json,junit:target/cucumber.xml")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.cucumber.automation")
 public class RunCucumberTest {
 }
