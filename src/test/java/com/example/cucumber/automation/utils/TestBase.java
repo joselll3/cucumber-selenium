41bdfb08816c61f2c1dce6ff9518ad835e83257d
Jose <jllopezl07@gmail.com>
2022-11-24 17:19:59 +0100

    Parameters in properties file

--- src/test/java/com/example/cucumber/automation/utils/TestBase.java
+++ src/test/java/com/example/cucumber/automation/utils/TestBase.java
@@ -1,18 +1,40 @@
 package com.example.cucumber.automation.utils;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
 
+	private static final String GLOBAL_PROPERTIES = "global.properties";
+	private static final String PROPERTY_BASE_URL = "baseUrl";
+	private static final String PROPERTY_BROWSER = "browser";
+	private static final String BROWSER_CHROME = "chrome";
+
 	private WebDriver driver;
 
 	public WebDriver webDriverManager() {
 		if (driver == null) {
-			driver = new ChromeDriver();
-			driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+			Properties properties = loadProperties();
+			if (properties.getProperty(PROPERTY_BROWSER).equalsIgnoreCase(BROWSER_CHROME)) {
+				System.setProperty("webdriver.chrome.driver",
+						Thread.currentThread().getContextClassLoader().getResource("chromedriver.exe").getFile());
+				driver = new ChromeDriver();
+			}
+			driver.get(properties.getProperty(PROPERTY_BASE_URL));
 		}
 		return driver;
 	}
 
+	private Properties loadProperties() {
+		Properties properties = new Properties();
+		try {
+			properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(GLOBAL_PROPERTIES));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return properties;
+	}
 }
