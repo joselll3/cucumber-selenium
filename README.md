4677c129edbe24372635470cec9713eeb2b99fc1
Jose <jllopezl07@gmail.com>
2022-11-28 16:11:15 +0100

    take snapshot from failure on scenario

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Getting started using cucumber
 ## Description
 
 see https://github.com/cucumber/cucumber-java-skeleton for initial config
+download chrome driver from https://chromedriver.chromium.org/downloads and put into src/test/resources
 
 ## Examples
 https://rahulshettyacademy.com/seleniumPractise/#/
--- src/test/java/com/example/cucumber/automation/stepdefinitions/Hooks.java
+++ src/test/java/com/example/cucumber/automation/stepdefinitions/Hooks.java
@@ -1,8 +1,14 @@
 package com.example.cucumber.automation.stepdefinitions;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import com.example.cucumber.automation.utils.TestContextSetup;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
 
 public class Hooks {
 
@@ -16,6 +22,17 @@ public class Hooks {
 	@After
 	public void afterScenario() {
 		testContextSetup.getTestBase().webDriverManager().quit();
+	}
+
+	@AfterStep
+	public void addScreenShot(Scenario scenario) {
+		if (scenario.isFailed()) {
+			WebDriver webDriver = testContextSetup.getTestBase().webDriverManager();
+
+			byte[] screenShot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(screenShot, "image/png", "image");
+
+		}
 	}
 
 }
