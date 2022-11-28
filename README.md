d84c5ecf2569f051016d95776693a40a780e2a04
Jose <jllopezl07@gmail.com>
2022-11-28 15:22:07 +0100

    extentreports-cucumber7-adapter

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ see https://github.com/cucumber/cucumber-java-skeleton for initial config
 
 ## Examples
 https://rahulshettyacademy.com/seleniumPractise/#/
+
+## Extend reports
+https://github.com/grasshopper7/extentreports-cucumber7-adapter
--- src/test/java/com/example/cucumber/automation/RunCucumberTest.java
+++ src/test/java/com/example/cucumber/automation/RunCucumberTest.java
@@ -12,7 +12,8 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber.html,json:target/cucumber.json,junit:target/cucumber.xml")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber.html,json:target/cucumber.json,junit:target/cucumber.xml,"
+		+ "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.cucumber.automation")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not (@AcademicTest)")
 public class RunCucumberTest {
